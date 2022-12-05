10120aa7f6a66cd82e3075e4bd99b7fd77e326d8
GiHwan2 <sandwich5011@naver.com>
2022-12-05 21:08:14 +0900

    스케줄 선택 UI 디자인

--- GOiOS/ContentView.swift
+++ GOiOS/ContentView.swift
@@ -8,12 +8,79 @@
 import SwiftUI
 
 struct ContentView: View {
+    let columns: [GridItem] = [
+        GridItem(.fixed(40), spacing: 5, alignment: nil),
+        GridItem(.fixed(40), spacing: 5, alignment: nil),
+        GridItem(.fixed(40), spacing: 5, alignment: nil),
+        GridItem(.fixed(40), spacing: 5, alignment: nil),
+        GridItem(.fixed(40), spacing: 5, alignment: nil),
+        GridItem(.fixed(40), spacing: 5, alignment: nil),
+        GridItem(.fixed(40), spacing: 5, alignment: nil)
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack{
+            HStack {
+                Text("Unavailable")
+                Rectangle()
+                      .fill(Color.red)
+                      .frame(width: 40, height: 40)
+                
+                Spacer()
+                Text("available")
+                Rectangle()
+                      .fill(Color.blue)
+                      .frame(width: 40, height: 40)
+            }
+            .font(.title3)
+            Spacer()
+            HStack(alignment: .center, spacing:13){
+                Text("Mon")
+                Text("Tue")
+                Text("Wed")
+                Text("Thu")
+                Text("Fri")
+                Text("Sat")
+                Text("Sun")
+            }
+            .font(.title3)
+            .padding(.leading,50)
+            HStack{
+                VStack(alignment: .center, spacing:25){
+                    Group{
+                        Text("09:00")
+                        Text("10:00")
+                        Text("11:00")
+                        Text("12:00")
+                        Text("01:00")
+                        Text("02:00")
+                        Text("03:00")
+                        Text("04:00")
+                        Text("05:00")
+                        Text("06:00")
+                    }
+                    Group{
+                        Text("07:00")
+                        Text("08:00")
+                        Text("09:00")
+                        Text("10:00")
+                        Text("11:00")
+                        Text("12:00")
+                    }
+                }.font(.system(size: 10))
+                LazyVGrid(columns: columns,spacing: 0) {
+                    ForEach(0..<105) { index in
+                        Rectangle()
+                            .fill(Color.white)
+                            .frame(height: 37)
+                            .border(Color.black, width: 1)
+                    }
+                    
+                }
+            }
+            
+            Spacer()
+            
         }
         .padding()
     }
